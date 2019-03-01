d11dd6806ffe7f188990993546fd30db35be10ff
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2019-03-01 13:22:00 +0100

    [nrf noup] cmake: multi image build support with IMAGE prefix
    Note: This was originally submitted upstream in #13672 and was
          rejected. NCS is carrying it as a noup for now. Another,
          more upstream-friendly approach will be needed.
    
    We are seeing the need to boot Zephyr applications in multiple
    stages, a real-world use-case of even a 3-stage bootloader has
    been identified and tested.
    
    Each bootloader needs to be individually upgrade-able and have
    it's own configurations of Zephyr libraries. To achieve this each
    bootloader is organized as it's own executable.
    
    The problem is that the build system can only build one
    executable. This creates a usability issue as the user must invoke a
    large set of arcane commands to build, sign, and flash each
    bootloader.
    
    To resolve this we re-organize the build system such that it can build
    multiple executables. To work within CMake restrictions that logical
    target names must be globally unique (among other naming
    restrictions), we add an IMAGE variable which is used to name the
    current Zephyr application being built.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    Signed-off-by: Ruth Fuchss <ruth.fuchss@nordicsemi.no>
    
    (cherry picked from commit 3db22acba35aa62abb18f4823102401f8b211a57)
    (Conflicts were resolved in lib/posix/CMakeLists.txt. Changes to
     ia32.cmake were dropped entirely since Intel has rejected this
     approach.
    
     Finally, these follow-up fixes and dependent patches were squashed
     in:
    
      02eeeb779665f126f59c185430d854bc7392f18e
      c6c3fed492e0f49679f5c6230dfc955c72f4ffaf
      56f6a33a71c46ec5b2e1beb6076edf9521014ad3
      1dc0ca5ccead37a53f600c65a941938f4138cf38
      6e348b8e609109981cbc140f06c324d938b52cd4)
    Signed-off-by: Marti Bolivar <marti.bolivar@nordicsemi.no>
    
    (cherry picked from commit 1e0a3dc603d9cbb9969563f1cbf308b147582461)
    (Conflict resolved in cmake/app/boilerplate.cmake)
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    (cherry picked from commit 439a1ad80b7618ae58477fe75477670d44f654d8)
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>
    (cherry picked from commit bbcbd8f01614bb341061e3074a0c76b8adf080d5)
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- cmake/dts.cmake
+++ cmake/dts.cmake
@@ -16,7 +16,7 @@ set(GENERATED_DTS_BOARD_UNFIXED_H ${PROJECT_BINARY_DIR}/include/generated/genera
 set(GENERATED_DTS_BOARD_CONF      ${PROJECT_BINARY_DIR}/include/generated/generated_dts_board.conf)
 set(DTS_POST_CPP                  ${PROJECT_BINARY_DIR}/${BOARD}.dts.pre.tmp)
 
-set_ifndef(DTS_SOURCE ${BOARD_DIR}/${BOARD}.dts)
+set_ifndef(${IMAGE}DTS_SOURCE ${BOARD_DIR}/${BOARD}.dts)
 
 if(DEFINED DTS_COMMON_OVERLAYS)
   # TODO: remove this warning in version 1.16
@@ -37,22 +37,22 @@ list(REMOVE_DUPLICATES
   )
 
 set(dts_files
-  ${DTS_SOURCE}
+  ${${IMAGE}DTS_SOURCE}
   ${shield_dts_files}
   )
 
 # TODO: What to do about non-posix platforms where NOT CONFIG_HAS_DTS (xtensa)?
 # Drop support for NOT CONFIG_HAS_DTS perhaps?
-if(EXISTS ${DTS_SOURCE})
+if(EXISTS ${${IMAGE}DTS_SOURCE})
   set(SUPPORTS_DTS 1)
 else()
   set(SUPPORTS_DTS 0)
 endif()
 
 if(SUPPORTS_DTS)
-  if(DTC_OVERLAY_FILE)
+  if(${IMAGE}DTC_OVERLAY_FILE)
     # Convert from space-separated files into file list
-    string(REPLACE " " ";" DTC_OVERLAY_FILE_AS_LIST ${DTC_OVERLAY_FILE})
+    string(REPLACE " " ";" DTC_OVERLAY_FILE_AS_LIST ${${IMAGE}DTC_OVERLAY_FILE})
     list(APPEND
       dts_files
       ${DTC_OVERLAY_FILE_AS_LIST}
